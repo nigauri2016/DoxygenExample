5e7641f5a8b3a67202a0826e82389961585e562e
Philip J Fry <someone@example.com>
2019-08-15 14:28:17 +0800

    add example of doxygen style code

---
+++ Class.cpp
@@ -0,0 +1,31 @@
+/// \file Class.cpp
+/// source file for class Ctest.
+/// 
+/// A detailed file description.
+///
+/// \author author_name
+/// \version version_number
+/// \date xxxx-xx-xxx
+
+#include "ClassHeader.h"
+
+/// A detailed namespace description, it
+/// should be 2 lines at least.
+namespace MyNamespace
+{
+  
+   /// constructor detailed defintion, 
+   /// should be 2 lines
+   MyClass::MyClass()
+     :PublicVariable(10)
+   {
+
+   }
+
+   /// A detailed function description, it
+   /// should be 2 lines at least.
+   int MyClass::GetVariable(int p1, float p2)
+   {
+       return 1;
+   }
+}
---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include "ClassHeader.h"
+
+using namespace MyNameSpace;
+
+int main(int argc, char* argv[])
+{
+    auto myClass = new MyClass();
+    return 0;
+}
